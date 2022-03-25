6d4c15551da46cfda6fc4bc5b83e45d3ead32d6a
cheerfulleg <yolginivan@gmail.com>
2022-03-25 15:27:51 +0200

    implementing Project functionality

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -8,15 +8,26 @@ import "@lourenci/react-kanban/dist/styles.css";
 import BoardCard from "../components/BoardCard";
 import findNewCard from "../services/utils";
 import ColumnAdder from "../components/ColumnAdder";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import {a11yProps, TabPanel} from "../components/TabPanel";
+import AddIcon from '@mui/icons-material/Add';
+import Button from "@mui/material/Button";
+import MemberItem from "../components/MemberItem";
 
 
 function Project() {
     const [project, setProject] = useState('')
+    const [projectMembers, setProjectMembers] = useState([])
+    const [value, setValue] = useState(0)
     const params = useParams()
 
     useEffect(() => {
         axios.get(`/projects/${params.id}`)
-            .then(res => setProject(res.data))
+            .then(res => {
+                setProject(res.data)
+                setProjectMembers(res.data.team_members)
+            })
             .catch(e => console.log(e))
     }, [])
 
@@ -55,50 +66,100 @@ function Project() {
             .catch(err => console.log(err))
     }
 
-    return (
-        <Box>
-            Project
-            <Typography>{project.id}</Typography>
-            <Typography>{project.name}</Typography>
-            <Typography>{project.description}</Typography>
-            {project ?
-                <Board
-                    allowAddColumn
-                    allowRemoveCard
-                    disableColumnDrag
-                    allowRemoveColumn
-                    onCardDragEnd={handleDragEnd}
-                    onCardRemove={handleCardRemove}
-                    onLaneRename={console.log}
-                    initialBoard={project}
-                    allowAddCard={{on: "top"}}
+    const handleChange = (event, value) => {
+        setValue(value)
+    }
 
-                    renderCard={(content, {removeCard, dragging}) =>
-                        <BoardCard card={content}
-                                   dragging={dragging}
-                                   removeCard={removeCard}
-                                   projectId={project.id}
-                                   members={project.team_members}
+    const handleRemoveMember = (id) => {
+        const newList = projectMembers.filter(item => item.id !== id);
+        setProjectMembers(newList)
+        axios.delete(`/projects/${project.id}/member/${id}`)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+
+    const handleUpdateMember = (id, data) => {
+        axios.patch(`/projects/${project.id}/member/${id}`, data)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+    const handleAssignMember = (card, value) => {
+        axios.patch(`/projects/${project.id}/task/${card.id}/assign`,
+            {assignee_id: value.id})
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+
+    if (project) {
+        return (
+            <Box>
+                <Typography variant='h3'>{project.name}</Typography>
+                <Typography variant='body1'>{project.description}</Typography>
+                <Box sx={{
+                    marginTop: 2,
+                    borderBottom: 1,
+                    borderColor: 'divider'
+                }}>
+                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                        <Tab label="Board" {...a11yProps(0)} />
+                        <Tab label="Team" {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
+                <TabPanel value={value} index={0}>
+
+                    <Board
+                        allowAddColumn
+                        allowRemoveCard
+                        disableColumnDrag
+                        allowRemoveColumn
+                        onCardDragEnd={handleDragEnd}
+                        onCardRemove={handleCardRemove}
+                        onLaneRename={console.log}
+                        initialBoard={project}
+                        allowAddCard={{on: "top"}}
+
+                        renderCard={(content, {removeCard, dragging}) =>
+                            <BoardCard card={content}
+                                       dragging={dragging}
+                                       removeCard={removeCard}
+                                       assignMember={handleAssignMember}
+                                       members={projectMembers}
+                            />
+                        }
+                        onNewCardConfirm={(draftCard) => ({
+                            id: new Date().getTime(),
+                            ...draftCard
+                        })}
+                        onCardNew={handleCardAdd}
+
+                        onNewColumnConfirm={(newColumn) => ({
+                            id: new Date().getTime(),
+                            ...newColumn
+                        })}
+
+                        onColumnNew={handleAddColumn}
+                        renderColumnAdder={({addColumn}) => <ColumnAdder addColumn={addColumn}/>}
+                        onColumnRemove={handleColumnRemove}
+                    />
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    <Button variant="outlined">
+                        <AddIcon/>  add teammate
+                    </Button>
+                    {projectMembers.map(member =>
+                        <MemberItem key={member.user.id}
+                                    elevation={2}
+                                    member={member}
+                                    onRemove={handleRemoveMember}
+                                    onUpdate={handleUpdateMember}
                         />
-                    }
-                    onNewCardConfirm={(draftCard) => ({
-                        id: new Date().getTime(),
-                        ...draftCard
-                    })}
-                    onCardNew={handleCardAdd}
-
-                    onNewColumnConfirm={(newColumn) => ({
-                        id: new Date().getTime(),
-                        ...newColumn
-                    })}
-
-                    onColumnNew={handleAddColumn}
-                    renderColumnAdder={({addColumn}) => <ColumnAdder addColumn={addColumn}/>}
-                    onColumnRemove={handleColumnRemove}
-                /> : null
-            }
-        </Box>
-    )
+                    )}
+                </TabPanel>
+            </Box>
+        )
+    } else {
+        return <>Loading...</>
+    }
 
 }
 
